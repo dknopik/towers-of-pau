ba79808d12077661504ba39cefa0ef255d8c89e4
Daniel Knopik <daniel.knopik@stud.tu-darmstadt.de>
2022-06-11 13:50:04 +0200

    cmc/coordinator: move implementation and finish RetrieveParticipant

--- cmd/coordinator/main.go
+++ cmd/coordinator/main.go
@@ -1,28 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"github.com/dknopik/towersofpau"
 	"github.com/gorilla/mux"
 	"log"
 	"math/rand"
 	"net/http"
 	"os"
-	"sync"
 	"time"
-)
-
-var mutex sync.Mutex
-var slotByTicket = make(map[string]*slot)
-var slots = make([]*slot, 0)
-var currentSlot = 0
-var ceremony *towersofpau.Ceremony = nil
-
-const (
-	participantTime     = 20
-	coordinatorTime     = 120
-	immediateStartDelay = 5
-	pushbackDelay       = 10
 )
 
 func main() {
@@ -31,111 +16,20 @@ func main() {
 	if err != nil {
 		log.Fatal("unable to open")
 	}
-	ceremony, err = towersofpau.Deserialize(file)
+	ceremony, err := towersofpau.Deserialize(file)
 	if err != nil {
 		log.Fatal("unable to decode", err.Error())
 	}
+	coordinator := NewCoordinator(ceremony)
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/participation", registerParticipant).
+	router.HandleFunc("/participation", coordinator.RegisterParticipant).
 		Methods("POST")
-	router.HandleFunc("/participation/{ticket}", retrieveParticipant).
+	router.HandleFunc("/participation/{ticket}", coordinator.RetrieveParticipant).
 		Methods("GET")
-	router.HandleFunc("/participation/{ticket}", submitCeremony).
+	router.HandleFunc("/participation/{ticket}", coordinator.SubmitCeremony).
 		Methods("POST")
 	err = http.ListenAndServe(":2016", router)
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func registerParticipant(rw http.ResponseWriter, req *http.Request) {
-	mutex.Lock()
-	defer mutex.Unlock()
-	slot := new(slot)
-	slot.index = len(slots)
-	if currentSlot == slot.index {
-		slot.start = time.Now().Unix() + immediateStartDelay
-	} else {
-		slot.start = slots[len(slots)-1].deadline + coordinatorTime
-	}
-	slot.deadline = slot.start + participantTime
-	slot.participantTicket = getTicket()
-	slotByTicket[slot.participantTicket] = slot
-	resp, err := json.Marshal(towersofpau.RegistrationResponse{
-		Start:    slot.start,
-		Deadline: slot.deadline,
-		Ticket:   slot.participantTicket,
-	})
-	if err != nil {
-		rw.WriteHeader(500)
-		return
-	}
-	rw.Write(resp)
-}
-
-const ticketBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-
-func getTicket() string {
-	b := make([]byte, 32)
-	for i := range b {
-		b[i] = ticketBytes[rand.Intn(len(ticketBytes))]
-	}
-	return string(b)
-}
-
-func retrieveParticipant(rw http.ResponseWriter, req *http.Request) {
-	mutex.Lock()
-	defer mutex.Unlock()
-	ticket := mux.Vars(req)["ticket"]
-	slot := slotByTicket[ticket]
-	if slot == nil || slot.index < currentSlot {
-		rw.WriteHeader(403)
-		return
-	}
-
-	response := towersofpau.FetchResponse{
-		Start:    slot.start,
-		Deadline: slot.deadline,
-		Ceremony: nil,
-	}
-
-	// check if current slot has expired or is in processing
-	if currentSlot < slot.index {
-
-	}
-
-	if currentSlot == slot.index {
-		jsonceremony, err := towersofpau.SerializeJSONCeremony(ceremony)
-		if err != nil {
-			rw.WriteHeader(500)
-			return
-		}
-		response.Ceremony = &jsonceremony
-	}
-
-	if currentSlot < slot.index && slot.start < time.Now().Unix() {
-		for _, slot := range slots[currentSlot+1:] {
-			slot.start += pushbackDelay
-			slot.deadline += pushbackDelay
-		}
-	}
-
-	resp, err := json.Marshal(response)
-	if err != nil {
-		rw.WriteHeader(500)
-		return
-	}
-	rw.Write(resp)
-}
-
-func submitCeremony(rw http.ResponseWriter, req *http.Request) {
-
-}
-
-type slot struct {
-	index             int
-	start             int64
-	deadline          int64
-	participantTicket string
-	submitted         bool
 }
---
+++ cmd/coordinator/server.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/dknopik/towersofpau"
+	"github.com/gorilla/mux"
+	"math/rand"
+	"net/http"
+	"sync"
+	"time"
+)
+
+const (
+	participantTime     = 20
+	coordinatorTime     = 120
+	immediateStartDelay = 5
+	pushbackDelay       = 10
+)
+
+func NewCoordinator(ceremony *towersofpau.Ceremony) *Coordinator {
+	return &Coordinator{
+		mutex:        sync.Mutex{},
+		slotByTicket: make(map[string]*slot),
+		slots:        make([]*slot, 0),
+		currentSlot:  0,
+		ceremony:     ceremony,
+	}
+}
+
+type Coordinator struct {
+	mutex        sync.Mutex
+	slotByTicket map[string]*slot
+	slots        []*slot
+	currentSlot  int
+	ceremony     *towersofpau.Ceremony
+}
+
+func (c *Coordinator) RegisterParticipant(rw http.ResponseWriter, req *http.Request) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	slot := new(slot)
+	slot.index = len(c.slots)
+	if c.currentSlot == slot.index {
+		slot.start = time.Now().Unix() + immediateStartDelay
+	} else {
+		slot.start = c.slots[len(c.slots)-1].deadline + coordinatorTime
+	}
+	slot.deadline = slot.start + participantTime
+	slot.participantTicket = getTicket()
+	c.slotByTicket[slot.participantTicket] = slot
+	resp, err := json.Marshal(towersofpau.RegistrationResponse{
+		Start:    slot.start,
+		Deadline: slot.deadline,
+		Ticket:   slot.participantTicket,
+	})
+	if err != nil {
+		rw.WriteHeader(500)
+		return
+	}
+	rw.Write(resp)
+}
+
+const ticketBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+func getTicket() string {
+	b := make([]byte, 32)
+	for i := range b {
+		b[i] = ticketBytes[rand.Intn(len(ticketBytes))]
+	}
+	return string(b)
+}
+
+func (c *Coordinator) RetrieveParticipant(rw http.ResponseWriter, req *http.Request) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	ticket := mux.Vars(req)["ticket"]
+	slot := c.slotByTicket[ticket]
+	if slot == nil || slot.index < c.currentSlot {
+		rw.WriteHeader(403)
+		return
+	}
+
+	response := towersofpau.FetchResponse{
+		Start:    slot.start,
+		Deadline: slot.deadline,
+		Ceremony: nil,
+	}
+
+	// check if current slot has expired or is in processing
+	for c.currentSlot < slot.index {
+		if c.slots[c.currentSlot].submitted || c.slots[c.currentSlot].deadline >= time.Now().Unix() {
+			break
+		} else {
+			c.currentSlot++
+		}
+	}
+
+	if c.currentSlot == slot.index {
+		if slot.deadline < time.Now().Unix() {
+			c.currentSlot++
+			rw.WriteHeader(403)
+			return
+		} else {
+			jsonceremony, err := towersofpau.SerializeJSONCeremony(c.ceremony)
+			if err != nil {
+				rw.WriteHeader(500)
+				return
+			}
+			response.Ceremony = &jsonceremony
+		}
+	} else if c.currentSlot < slot.index && slot.start < time.Now().Unix() {
+		for _, slot := range c.slots[c.currentSlot+1:] {
+			slot.start += pushbackDelay
+			slot.deadline += pushbackDelay
+		}
+	}
+
+	resp, err := json.Marshal(response)
+	if err != nil {
+		rw.WriteHeader(500)
+		return
+	}
+	rw.Write(resp)
+}
+
+func (c *Coordinator) SubmitCeremony(rw http.ResponseWriter, req *http.Request) {
+
+}
+
+type slot struct {
+	index             int
+	start             int64
+	deadline          int64
+	participantTicket string
+	submitted         bool
+}
